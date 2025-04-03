de6bff894a9a5e2a5204cf29506da63d91d7fe37
matheus costa <dev.matheuspc@gmail.com>
2025-04-03 09:09:27 -0300

    lightprovider problema

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
   const { lightMode } = useLight()
   const bgLight = "absolute top-0 -z-[-2] antialiased overflow-x-hidden min-h-screen w-screen  bg-neutral-200 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"
   const bgDark = "absolute top-0 -z-[-2] text-neutral-50 antialiased overflow-x-hidden min-h-screen w-screen  bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"
+  
   return (
     <div className= {lightMode ? bgDark : bgLight}>
       <div className="container mx-auto px-15 lg:px-50 py-10 flex flex-col justify-center">
